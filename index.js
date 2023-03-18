54b55d7a49b94b55b3a253a8030a8e6732aabccc
Brendon Rofe <b1@xgmi.com>
2023-03-18 15:40:53 +0200

    Initial add post functionality

--- index.js
+++ index.js
@@ -6,14 +6,32 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 let nextId = 1;
+let nextPostId = 1;
 
 const users = [];
 
 class User {
+
   constructor(id, email, password) {
     this.id = id;
     this.email = email;
     this.password = password;
+    this.posts = [];
+  }
+
+  addPost(title, content) {
+    const newPost = new Post(nextPostId, title, content);
+    this.posts.push(newPost);
+    nextPostId++;
+  }
+
+}
+
+class Post {
+  constructor(id, title, content) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
   }
 }
 
@@ -28,12 +46,24 @@ const addUser = (req, res) => {
   res.json({ message: 'New user added' });
 };
 
+const addPost = (req, res) => {
+  let user;
+  for(i = 0; i < users.length; i++) {
+    if (users[i].id === req.body.id) {
+      user = users[i];
+    }
+  }
+  user.addPost(req.body.title, req.body.content);
+  res.json({ message: 'Post added' });
+}
+
 const getUsers = (req, res) => {
   res.json({ users: users });
 };
 
 app.post('/users', addUser);
 app.get('/users', getUsers);
+app.post('/posts', addPost);
 
 app.listen(3000, () => {
   console.log('App listening on port 3000');
