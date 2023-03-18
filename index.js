2f1333f639ac9462459d2294dbaf80d4171dc8f9
Brendon Rofe <b1@xgmi.com>
2023-03-18 15:10:19 +0200

    Add create user functionality

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-const users = [
-  { id: 1, email: 'brofe93@gmail.com', password: 'pass123' },
-  { id: 2, email: 'someguy@gmail.com', password: 'pass123' },
-  { id: 3, email: 'anotheruser@gmail.com', password: 'pass123' }
-];
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+let nextId = 1;
+
+const users = [];
+
+class User {
+  constructor(id, email, password) {
+    this.id = id;
+    this.email = email;
+    this.password = password;
+  }
+}
+
+const addUser = (req, res) => {
+  const newUser = new User(
+    nextId,
+    req.body.email,
+    req.body.password
+  );
+  users.push(newUser);
+  nextId++;
+  res.json({ message: 'New user added' });
+};
 
 const getUsers = (req, res) => {
   res.json({ users: users });
 };
 
+app.post('/users', addUser);
 app.get('/users', getUsers);
 
 app.listen(3000, () => {
