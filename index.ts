b8a175c1de0995c16b800d256557083d020c314a
Bravo1B9 <brofe93@gmail.com>
2023-04-03 11:02:29 +0000

    App reset

--- index.ts
+++ index.ts
@@ -1,22 +1,16 @@
-const userRouter = require('./routes/userRoutes');
-import { Request, Response } from "express";
-const database = require('./db');
+import { Application, Request, Response } from "express";
+
 const express = require('express');
 
-
-database.connectToDatabase();
-
+const app: Application = express();
 const PORT = 3000;
 
-const app = express();
-
-app.use(express.json());
-app.use('/api', userRouter);
-
-app.get('/', (req: Request, res: Response) => {
+const sayHello = (req: Request, res: Response) => {
   res.json({ msg: 'Hello world' });
-})
+};
+
+app.get('/', sayHello);
 
 app.listen(PORT, () => {
-  console.log(`App listening on port: ${PORT}`);
+  console.log(`Server listening on port: ${PORT}`);
 });
