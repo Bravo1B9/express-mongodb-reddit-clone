3c4ec44be1f321b0b1a6d5387436e76e6190fb5a
Bravo1B9 <brofe93@gmail.com>
2023-04-04 05:50:12 +0000

    API reset

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 import { Application, Request, Response } from "express";
 import { connectToDatabase } from "./db";
-const userRouter = require('./routes/userRoutes');
-const postRouter = require('./routes/postRoutes');
 
 const express = require('express');
 
@@ -15,7 +13,8 @@ app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
   res.json({ msg: 'Home Page' });
 });
-app.use('/api', userRouter, postRouter);
+
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
--- models/userModel.ts
+++
@@ -1,5 +0,0 @@
-export interface UserModal {
-  username: string;
-  email: string;
-  password: string;
-}
